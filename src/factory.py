32a6b832801f4a8bda7c3c8ad595fe288dafb878
FATESAIKOU <qzsecftbhhhh@gmail.com>
2017-10-14 15:09:10 +0800

    generator main sequence complete

---
+++ src/factory.py
@@ -0,0 +1,5 @@
+from pprint import pprint
+
+def makeTestCases(q_fac, cfg):
+    pprint(cfg)
+    return q_fac.inputs, cfg
--- src/run_gen.py
+++ src/run_gen.py
@@ -1,6 +1,40 @@
 #!/usr/bin/env python
 
+import sys
+import json
+
 import factory
 import q1fac, q2fac, q3fac
 
-print "hello world"
+def main():
+    q_name = sys.argv[1]
+    q_ifile = sys.argv[2]
+    q_ofile = sys.argv[3]
+    
+    q_cfg_src = open(sys.argv[4], 'r')
+    q_cfg = json.loads(q_cfg_src.read())
+    q_cfg_src.close()
+    
+    
+    if q_name == 'q1':
+        inputs, outputs = factory.makeTestCases(q1fac, q_cfg)
+    elif q_name == 'q2':
+        inputs, outputs = factory.makeTestCases(q2fac, q_cfg)
+    elif q_name == 'q3':
+        inputs, outputs = factory.makeTestCases(q3fac, q_cfg)
+    else:
+        print "<Black Man>???"
+        return 1
+    
+    
+    q_ifile_src = open(q_ifile, 'w')
+    q_ifile_src.write('\n'.join(inputs))
+    q_ifile_src.close()
+    
+    q_ofile_src = open(q_ofile, 'w')
+    q_ofile_src.write('\n'.join(outputs))
+    q_ofile_src.close()
+
+
+if __name__ == '__main__':
+    main()
